10bcbaea1af81f7e3bdcc579f59c338749c486fb
Abob888 <62065668+Abob888@users.noreply.github.com>
2021-05-03 16:45:21 +0300

    Add files via upload

---
+++ 20210503.py
@@ -0,0 +1,22 @@
+a = ['bag', 'som', 'rat', 'bat']
+b = ['нос', 'кот', 'рот', 'лот']
+c = ['123', '456', '789', '369']
+
+i = 0
+while i < len(a):
+    d = []
+    d.append(a[i])
+    d.append(b[i])
+    d.append(c[i])
+    print(d)
+    i = i + 1
+i = 0
+while i < len(a):
+    d1 = a[i]
+    d2 = b[i]
+    d3 = c[i]
+    print(d1 + ' ' + d2 + ' ' + d3)
+    i = i + 1
+
+    
+                
